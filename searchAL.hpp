230a2f10d9b1c7a3fe2ba741ecc319b39b7c1729
Kaczorsk <kaczorek@gmail.com>
2024-02-25 22:44:01 +0100

    dodanie plikow hpp

---
+++ searchAL.hpp
@@ -0,0 +1,3 @@
+int LinearSort(const int tab[], int size, int key);
+int wyszukiwanie_liniowe_z_wart(int tab[], int size, int key);
+int wyszukiwanie_binarne(int tab[], int size, int key);
---
+++ sortAL.hpp
@@ -0,0 +1,10 @@
+void Bubble_Sort(int tab[], int n);
+void sort_przez_wstawianie(int tab[], int n);
+void quickSort(int tab[], int lewy, int prawy);
+void Merge(int tab[], int lewy, int srodek, int prawy);
+void MergeSort(int tab[],int lewy, int prawy);
+void CoctailSort(int tab[], int n);
+
+
+
+
